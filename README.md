2bfe3265d4433ba0408a0b2c80e184a87d9a3cac
Andrew Denezhkin <a.denezhkin@bftcom.com>
2021-11-09 12:58:36 +0400

    Master Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# WebProject Test
+### Random Stydying Stuff
+
+<hr>
+There is nothing interesting here, only testing things with this stuff:
+
+<ol>
+    <li>Servlets</li>
+    <li>JSP</li>
+    <li>JDBC, etc.</li>
+</ol>
---
+++ src/main/java/ru/webproject/test1/TestServlet1.java
@@ -0,0 +1,17 @@
+package ru.webproject.test1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TestServlet1 extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("<h1>Hello!</h1>");
+        pw.println("<ol><li>test1</li><li>test2</li></ol");
+    }
+}
